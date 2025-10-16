f3bad781e972431adb5d5810e6e3852f65e5f780
Jami Khan <rjkhan.1221@gmail.com>
2025-10-16 23:03:25 +0600

    clean

--- backend/Google2FA.Api/Models/SetupResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace Google2FA.Api.Models
-{
-    public class SetupResponse
-    {
-        public string SecretKey { get; set; } = string.Empty;
-        public string QrCodeBase64 { get; set; } = string.Empty;
-    }
-}
